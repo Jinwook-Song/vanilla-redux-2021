a8d18caceba62b2a728cc0c62031f3b0c5e41b7d
jinwook-song <wlsdnr129@naver.com>
2021-04-21 17:04:13 +0900

    lecture 3.1 Connecting the store

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import { createStore } from "redux";
+
+// Action
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+export const deleteToDo = (id) => {
+  return {
+    type: ADD,
+    id,
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((toDo) => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
