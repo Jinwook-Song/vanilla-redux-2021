32d75a2593eb93b01c5bc842e57ebee3593dbda9
jinwook-song <wlsdnr129@naver.com>
2021-04-21 19:05:49 +0900

    toolkit. createReducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // Action with toolkit (caution: payload)
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
+/* normal reducer
 const reducer = (state = [], action) => {
   switch (action.type) {
     case addToDo.type:
@@ -14,7 +15,17 @@ const reducer = (state = [], action) => {
     default:
       return state;
   }
-};
+};*/
+
+// 주의: mutate 할 경우 절대로! return을 하면 안된다.
+// mutate state and doesn't return & return new state
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const store = createStore(reducer);
 
