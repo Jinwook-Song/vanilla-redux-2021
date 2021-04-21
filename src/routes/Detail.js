fe5808f184c33172553066198e3a8094cdecccea
jinwook-song <wlsdnr129@naver.com>
2021-04-21 18:15:41 +0900

    lecture 3.5 Detail Screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,22 @@
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created ad: {toDo?.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
 // eslint-disable-next-line
-export default () => "Detail";
+export default connect(mapStateToProps)(Detail);
